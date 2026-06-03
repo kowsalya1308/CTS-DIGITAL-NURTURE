d38d13cdaf99505dc1257a4588a1be7e075d7774
kowsalya1308 <kowsiammu0809@gmail.com>
2026-06-03 11:52:48 +0530

    Delete Exercise6.sql

--- Exercise6.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    e.event_id,
-    e.title,
-    COUNT(r.resource_id) AS total_resources
-FROM Events e
-LEFT JOIN Resources r
-    ON e.event_id = r.event_id
-GROUP BY e.event_id, e.title
-ORDER BY total_resources DESC;
