9ba16f5d564dcd148a9dadb89e6f88aeae7ba62b
kowsalya1308 <kowsiammu0809@gmail.com>
2026-06-03 11:53:29 +0530

    Delete Exercise3.sql

--- Exercise3.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    u.user_id,
-    u.full_name,
-    u.email
-FROM Users u
-LEFT JOIN Registrations r
-    ON u.user_id = r.user_id
-WHERE r.registration_date < '2025-04-01'
-   OR r.registration_date IS NULL;
